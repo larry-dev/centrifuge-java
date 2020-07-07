1c6bf9e406b4e05607a6c04b90038245b89f1aa1
larry <jclarry@qq.com>
2020-07-07 19:37:40 +0800

    修改参数获取错误

--- example/src/main/java/io/github/centrifugal/centrifuge/example/Main.java
+++ example/src/main/java/io/github/centrifugal/centrifuge/example/Main.java
@@ -33,50 +33,8 @@ public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         LOGGER.info("Initialize Client client");
-
-        EventListener listener = new EventListener() {
-            @Override
-            public void onConnect(Client client, ConnectEvent event) {
-                System.out.println("connected");
-            }
-
-            @Override
-            public void onDisconnect(Client client, DisconnectEvent event) {
-                System.out.printf("disconnected %s, reconnect %s%n", event.getReason(), event.getReconnect());
-            }
-
-            @Override
-            public void onError(Client client, ErrorEvent event) {
-                System.out.println("There was a problem connecting!");
-            }
-
-            @Override
-            public void onPrivateSub(Client client, PrivateSubEvent event, TokenCallback cb) {
-                cb.Done("boom");
-            }
-
-            @Override
-            public void onRefresh(Client client, RefreshEvent event, TokenCallback cb) {
-                cb.Done("boom");
-            }
-
-            @Override
-            public void onMessage(Client client, MessageEvent event) {
-                String data = new String(event.getData(), UTF_8);
-                System.out.println("message received: " + data);
-            }
-        };
-
-        Client client = new Client(
-                "ws://localhost:8000/connection/websocket?format=protobuf",
-                new Options(),
-                listener
-        );
-        client.setToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0c3VpdGVfand0In0.hPmHsVqvtY88PvK4EmJlcdwNuKFuy3BGaF7dMaKdPlw");
-        client.connect();
-
         SubscriptionEventListener subListener = new SubscriptionEventListener() {
             @Override
             public void onSubscribeSuccess(Subscription sub, SubscribeSuccessEvent event) {
@@ -120,89 +78,136 @@ public class Main {
                 System.out.println("client " + event.getInfo().getClient() + " left channel " + sub.getChannel());
             }
         };
+        EventListener listener = new EventListener() {
+            @Override
+            public void onConnect(Client client, ConnectEvent event) {
+                System.out.println("connected");
 
+
+            }
+
+            @Override
+            public void onDisconnect(Client client, DisconnectEvent event) {
+                System.out.printf("disconnected %s, reconnect %s%n", event.getReason(), event.getReconnect());
+            }
+
+            @Override
+            public void onError(Client client, ErrorEvent event) {
+                System.out.println("There was a problem connecting!");
+            }
+
+            @Override
+            public void onPrivateSub(Client client, PrivateSubEvent event, TokenCallback cb) {
+                cb.Done("boom");
+            }
+
+            @Override
+            public void onRefresh(Client client, RefreshEvent event, TokenCallback cb) {
+                cb.Done("boom");
+            }
+
+            @Override
+            public void onMessage(Client client, MessageEvent event) {
+                String data = new String(event.getData(), UTF_8);
+                System.out.println("message received: " + data);
+            }
+        };
+
+        Client client = new Client(
+                "ws://localhost:8080/connection/websocket?format=protobuf",
+                new Options(),
+                listener
+        );
+        //
+        client.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJhcHBfaWQiOjAsImlhdCI6MTU5NDEwMzQ5MywiaXNzIjoib25saW5lLWdhbWUifQ.ipBkFXTvUqcyU5m9Na1cFdYFk_436ZFsprRG0o85qr8");
+        String data="{\"input\": \"hi from Java\"}";
+        client.setConnectData(data.getBytes());
+        client.connect();
+        Thread.sleep(50000);
         Subscription sub;
         try {
-            sub = client.newSubscription("chat:index", subListener);
+            sub = client.newSubscription("game:95", subListener);
         } catch (DuplicateSubscriptionException e) {
             e.printStackTrace();
             return;
         }
         sub.subscribe();
 
-        String data="{\"input\": \"hi from Java\"}";
+        Thread.sleep(20000000L);
 
-        // Publish to channel (won't wait until client subscribed to channel).
-        // This message most probably won't be received by this client.
-        // You need to wait for subscription success to reliably receive
-        // publication - see publishing over sub object below.
-        client.publish("chat:index", data.getBytes(), new ReplyCallback<PublishResult>() {
-            @Override
-            public void onFailure(Throwable e) {
-                System.out.println("publish error: " + e);
-            }
-
-            @Override
-            public void onDone(ReplyError err, PublishResult reply) {
-                if (err != null) {
-                    System.out.println("error publish: " + err.getMessage());
-                    return;
-                }
-                System.out.println("successfully published");
-            }
-        });
-
-        // Publish via subscription (will wait for subscribe success before publishing).
-        sub.publish(data.getBytes(), new ReplyCallback<PublishResult>() {
-            @Override
-            public void onFailure(Throwable e) {
-                System.out.println("sub publish error: " + e);
-            }
-
-            @Override
-            public void onDone(ReplyError err, PublishResult res) {
-                if (err != null) {
-                    System.out.println("error publish: " + err.getMessage());
-                    return;
-                }
-                System.out.println("successfully published");
-            }
-        });
-
-        sub.presenceStats(new ReplyCallback<PresenceStatsResult>() {
-            @Override
-            public void onFailure(Throwable e) {
-                System.out.println("presence stats error: " + e);
-            }
-
-            @Override
-            public void onDone(ReplyError err, PresenceStatsResult res) {
-                if (err != null) {
-                    System.out.println("error presence stats: " + err.getMessage());
-                    return;
-                }
-                System.out.println("Num clients connected: " + res.getNumClients());
-            }
-        });
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-
-        sub.unsubscribe();
-
-        // Say Client that we finished with Subscription. It will be removed from
-        // internal Subscription map so you can create new Subscription to channel
-        // later calling newSubscription method again.
-        client.removeSubscription(sub);
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
+//        String data="{\"input\": \"hi from Java\"}";
+//
+//        // Publish to channel (won't wait until client subscribed to channel).
+//        // This message most probably won't be received by this client.
+//        // You need to wait for subscription success to reliably receive
+//        // publication - see publishing over sub object below.
+//        client.publish("chat:index", data.getBytes(), new ReplyCallback<PublishResult>() {
+//            @Override
+//            public void onFailure(Throwable e) {
+//                System.out.println("publish error: " + e);
+//            }
+//
+//            @Override
+//            public void onDone(ReplyError err, PublishResult reply) {
+//                if (err != null) {
+//                    System.out.println("error publish: " + err.getMessage());
+//                    return;
+//                }
+//                System.out.println("successfully published");
+//            }
+//        });
+//
+//        // Publish via subscription (will wait for subscribe success before publishing).
+//        sub.publish(data.getBytes(), new ReplyCallback<PublishResult>() {
+//            @Override
+//            public void onFailure(Throwable e) {
+//                System.out.println("sub publish error: " + e);
+//            }
+//
+//            @Override
+//            public void onDone(ReplyError err, PublishResult res) {
+//                if (err != null) {
+//                    System.out.println("error publish: " + err.getMessage());
+//                    return;
+//                }
+//                System.out.println("successfully published");
+//            }
+//        });
+//
+//        sub.presenceStats(new ReplyCallback<PresenceStatsResult>() {
+//            @Override
+//            public void onFailure(Throwable e) {
+//                System.out.println("presence stats error: " + e);
+//            }
+//
+//            @Override
+//            public void onDone(ReplyError err, PresenceStatsResult res) {
+//                if (err != null) {
+//                    System.out.println("error presence stats: " + err.getMessage());
+//                    return;
+//                }
+//                System.out.println("Num clients connected: " + res.getNumClients());
+//            }
+//        });
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException ex) {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        sub.unsubscribe();
+//
+//        // Say Client that we finished with Subscription. It will be removed from
+//        // internal Subscription map so you can create new Subscription to channel
+//        // later calling newSubscription method again.
+//        client.removeSubscription(sub);
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException ex) {
+//            Thread.currentThread().interrupt();
+//        }
         // Disconnect from server.
         client.disconnect();
     }
